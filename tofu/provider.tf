cd4f6841cf18d94f87911f27c2debc6c7f19191d
FXHibon <francois-xavier.hibon@iadvize.com>
2026-03-22 19:07:09 +0100

    vps: unstable

--- tofu/provider.tf
+++ tofu/provider.tf
@@ -2,7 +2,7 @@ terraform {
   required_providers {
     ovh = {
       source  = "ovh/ovh"
-      version = "2.11.0"
+      version = "2.12.0"
     }
   }
 }
@@ -13,3 +13,24 @@ provider "ovh" {
   application_secret = var.ovh_application_secret
   consumer_key       = var.ovh_consumer_key
 }
+
+/*
+# After creating the bucket and the s3_user with 'tofu apply', 
+# you can configure the remote backend like this.
+# You'll need to use 'tofu init -backend-config="access_key=..." -backend-config="secret_key=..."'
+# to pass the credentials without storing them in this file.
+
+terraform {
+  backend "s3" {
+    bucket                      = "tofu-state-bucket"
+    key                         = "tofu.tfstate"
+    region                      = "GRA" # Must match your OVH region
+    endpoint                    = "https://s3.gra.cloud.ovh.net" # Update based on region
+    skip_credentials_validation = true
+    skip_region_validation      = true
+    skip_requesting_account_id  = true
+    skip_metadata_api_check     = true
+    skip_s3_checksum            = true
+  }
+}
+*/
--- tofu/variables.tf
+++ tofu/variables.tf
@@ -9,3 +9,18 @@ variable "ovh_application_secret" {
 variable "ovh_consumer_key" {
   type = string
 }
+
+variable "ovh_cloud_project_id" {
+  type        = string
+  description = "The ID of the OVH Public Cloud project (service_name)"
+}
+
+variable "ovh_region" {
+  type    = string
+  default = "GRA"
+}
+
+locals {
+  # This is the IP address of my home network (Freebox)
+  home_ip_v4 = "REDACTED_HOME_IP_OLD"
+}
