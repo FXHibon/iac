14e881d91328910d0e814ad23a14c5fbb553493d
FXHibon <francois-xavier.hibon@iadvize.com>
2026-05-25 11:35:54 +0200

    feat: add prometheus/node-exporter/grafana monitoring stack on VPS and integrate traefik metrics

--- tofu/vps/ovh/dns.tf
+++ tofu/vps/ovh/dns.tf
@@ -46,6 +46,24 @@ resource "ovh_domain_zone_record" "traefik_vps_record_v4" {
   target    = tolist(data.ovh_vps.main_vps.ips)[1]
 }
 
+# Prometheus Dashboard Entry (prometheus.vps.fxhibon.fr)
+resource "ovh_domain_zone_record" "prometheus_vps_record_v4" {
+  zone      = ovh_domain_name.root_domain.domain_name
+  subdomain = "prometheus.vps"
+  fieldtype = "A"
+  ttl       = 3600
+  target    = tolist(data.ovh_vps.main_vps.ips)[1]
+}
+
+# Grafana Dashboard Entry (grafana.vps.fxhibon.fr)
+resource "ovh_domain_zone_record" "grafana_vps_record_v4" {
+  zone      = ovh_domain_name.root_domain.domain_name
+  subdomain = "grafana.vps"
+  fieldtype = "A"
+  ttl       = 3600
+  target    = tolist(data.ovh_vps.main_vps.ips)[1]
+}
+
 # Wildcard for all other services on the VPS (e.g. *.vps.fxhibon.fr)
 resource "ovh_domain_zone_record" "wildcard_vps_record_v4" {
   zone      = ovh_domain_name.root_domain.domain_name
